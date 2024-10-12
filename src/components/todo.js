6d45c93d421935f207afd522b6eec1df7e1a6808
Felipe dos Santos Bento <dsbfelipe@outlook.com>
2024-10-11 23:13:33 -0300

    Create homepage design

---
+++ src/components/todo.js
@@ -0,0 +1,30 @@
+    <div class="todo">
+      <div class="checkbox-and-title">
+        <div>
+          <input class="checkbox" type="checkbox" />
+          <p class="todo-title">Todo example</p>
+        </div>
+        <p class="todo-priority">Important</p>
+      </div>
+      <div class="due-date">
+        Due date: 15 Out 2024
+      </div>
+      <div class="todo-line"></div>
+      <div class="todo-description">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos 
+        quo, nihil officiis beatae.
+      </div>
+      <div class="todo-line"></div>
+      <div class="todo-buttons">
+        <button class="todo-edit">
+          <img class="todo-buttons-icon" src="../public/svg/pencil.svg" alt="" />
+          Edit
+        </button>
+        <button class="todo-delete">
+          <img class="todo-buttons-icon" src="../public/svg/trash.svg" alt="" />
+          Delete
+        </button>
+      </div>
+    </div>
+
+
